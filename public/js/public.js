2b66d2cfec0df54266ee24d7d84a5f4b1f8db42d
Nicolas <nicoinginfo@gmail.com>
2025-05-17 15:25:41 -0400

    pantalla publica hecha

--- public/js/public.js
+++ public/js/public.js
@@ -1,3 +1,23 @@
 
+function renderTickets(tickets = []) {
 
-console.log('Público HTML')
+    for(let i = 0; i < tickets.length; i++) {
+        if( i>= 4) break;
+        const ticket = tickets[i];
+        if(!ticket) continue;
+
+        const lblTicket = document.querySelector(`#lbl-ticket-0${i+1}`);
+        const lblDesk = document.querySelector(`#lbl-desk-0${i+1}`);
+
+        lblTicket.innerText = `Ticket ${ticket.number}`;
+        lblDesk.innerText = ticket.handleAtDesk;
+    }
+}
+
+
+async function loadCurrentTickets() {
+    const tickets = await fetch('/api/ticket/working-on').then(resp => resp.json());
+    renderTickets(tickets);
+}
+
+loadCurrentTickets();
